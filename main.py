fbf579bed685df44b25890ae6cbb0218762a0459
jeewnish <jeewekan098@gmail.com>
2024-05-22 22:35:55 +0530

    The Miles to Km app.

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
-print("Hello Hello!!")
-assasin = "Tommie"
-print(assasin)
+import tkinter as tk
+# from tkinter import ttk
+import ttkbootstrap as ttk
+
+# function
+def convert():
+    mile_input = entry_int.get()
+    km_output = mile_input * 1.61
+    output_string.set(f"{km_output:.2f}Km")
+
+# Window
+window = ttk.Window(themename='vapor')
+window.title('Converter')
+window.geometry('360x200')
+
+# title
+title_label = ttk.Label(master=window, text='Miles to Kilometers', font='Calibri 24 bold')
+title_label.pack()
+
+# input field
+input_frame = ttk.Frame(master=window)
+entry_int = tk.IntVar()
+entry = ttk.Entry(master=input_frame, textvariable=entry_int, font='Calibri 20', width=5)
+miles_label = ttk.Label(master=input_frame, text='Miles', font='Calibri 30')
+button = ttk.Button(master=input_frame, text='Convert', command=convert)
+
+entry.pack(side='left')
+miles_label.pack(side='left')
+button.pack(side='left', padx=10)
+
+input_frame.pack(pady=10)
+
+# output
+output_string = tk.StringVar()
+output_label = ttk.Label(master=window, text='Output', font='Calibri 30', textvariable=output_string)
+output_label.pack(pady=12)
+
+# run
+window.mainloop()
